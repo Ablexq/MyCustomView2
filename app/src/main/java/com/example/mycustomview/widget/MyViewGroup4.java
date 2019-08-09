b9aabdaf7a8d9092b8991ebbb8e8048b3ce35db2
ablexq <776356314@qq.com>
2019-08-09 18:17:21 +0800

    md

--- app/src/main/java/com/example/mycustomview/widget/MyViewGroup4.java
+++ app/src/main/java/com/example/mycustomview/widget/MyViewGroup4.java
@@ -2,14 +2,36 @@ package com.example.mycustomview.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Scroller;
 
 /**
- * 自定义ViewGroup
- * 很简单的横向布局，把所有的子View都横着排列起来，不可滚动
+ * 自定义ViewGroup 快速滑动 支持margin
+ * https://blog.csdn.net/qq_18148011/article/details/53761576
  */
 public class MyViewGroup4 extends ViewGroup {
+
+    private Context context;
+    //滚动计算辅助类
+    private Scroller mScroller;
+    //手指落点的X坐标
+    private float mLastMotionX = 0;
+    //屏幕宽度
+    private int screenWidth;
+
+    //手指加速度辅助类
+    private VelocityTracker mVelocityTracker;
+    //每秒移动的最小dp
+    private int mMinimumVelocity;
+    //每秒移动的最大dp
+    private int mMaximumVelocity;
+
     public MyViewGroup4(Context context) {
         this(context, null);
     }
@@ -20,11 +42,22 @@ public class MyViewGroup4 extends ViewGroup {
 
     public MyViewGroup4(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
         init();
     }
 
     private void init() {
+        mScroller = new Scroller(context);
 
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(outMetrics);
+        screenWidth = outMetrics.widthPixels;
+
+        //获取最小和最大的移动距离
+        final ViewConfiguration configuration = ViewConfiguration.get(context);
+        mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
+        mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
     }
 
     @Override
@@ -103,12 +136,12 @@ public class MyViewGroup4 extends ViewGroup {
         int height = getHeight();
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
-            View child=getChildAt(i);
-            MarginLayoutParams lp= (MarginLayoutParams) child.getLayoutParams();
-            childWidth=child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
+            View child = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
             //最主要的一句话
-            child.layout(childLeft,0,childLeft+childWidth,height);
-            childLeft+=childWidth;
+            child.layout(childLeft, 0, childLeft + childWidth, height);
+            childLeft += childWidth;
         }
     }
 
@@ -116,6 +149,112 @@ public class MyViewGroup4 extends ViewGroup {
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    /*
+     * 滚动时需要重写的方法，用于控制滚动
+     * */
+//    @Override
+//    public void computeScroll() {
+//        if (mScroller.computeScrollOffset()) {
+//            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+//            postInvalidate();
+//        }
+//    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        //获取现在手指所在的位置的x坐标
+        float x = event.getX();
+        //判断触发的时间
+        switch (action) {
+            //按下事件
+            case MotionEvent.ACTION_DOWN:
+                //初始化或服用加速度测试器
+                initOrResetVelocityTracker();
+                //测试器添加按下事件
+                mVelocityTracker.addMovement(event);
+                //如果停止滚动则取消动画（即手指按下就停止滚动）
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                }
+                //获取现在的x坐标
+                mLastMotionX = event.getX();
+                break;
+            //移动事件
+            case MotionEvent.ACTION_MOVE:
+                //测试器添加移动事件
+                if (mVelocityTracker != null) {
+                    mVelocityTracker.addMovement(event);
+                }
+                //计算移动的偏移量
+                float delt = mLastMotionX - x;
+                //重置手指位置
+                mLastMotionX = x;
+                //滚动
+                scrollBy((int) delt, 0);
+                break;
+            //手指抬起事件
+            case MotionEvent.ACTION_UP:
+                //测试器添加抬起事件
+                mVelocityTracker.addMovement(event);
+                //添加加速度的测试时间，这里是测量1000毫秒内的加速度
+                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                //获取x方向加速度
+                float xVelocity = mVelocityTracker.getXVelocity();
+                //得到最后一个子View
+                View lastChild = getChildAt(getChildCount() - 1);
+                //获取滑动的最大滑动距离（最后一个Child的右边框的坐标减去屏幕的宽度）
+                int finallyChild = (int) (lastChild.getX() + lastChild.getWidth() - screenWidth);
+
+                //如果x的加速度大于系统设定的最小移动距离，就可以惯性滑动
+                if (Math.abs(xVelocity) > mMinimumVelocity)
+                    mScroller.fling(getScrollX(),
+                            0, (int) -xVelocity, 0,
+                            0, finallyChild,
+                            0, 0);
+
+                //如果滑动的距离小于第一个控件的最左边（0）则回弹至（0,0）点
+                if (getScrollX() < 0) {
+                    scrollTo(0, 0);
+                }
+                //如果滑动的距离大于最大可滑动距离则滑动到最后一个子View
+                if (getScrollX() >= finallyChild) {
+                    scrollTo(finallyChild, 0);
+                }
+
+                //刷新界面
+                invalidate();
+                //清空测试器
+                recycleVelocityTracker();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    /**
+     * 创建或复用加速度测试器
+     */
+    private void initOrResetVelocityTracker() {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        } else {
+            mVelocityTracker.clear();
+        }
+    }
+
+    /**
+     * 回收加速度测试器，防止内存泄漏
+     */
+    private void recycleVelocityTracker() {
+        if (mVelocityTracker != null) {
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
     }
 }
 
--- app/src/main/java/com/example/mycustomview/widget/MyViewGroup5.java
+++ app/src/main/java/com/example/mycustomview/widget/MyViewGroup5.java
@@ -3,8 +3,30 @@ package com.example.mycustomview.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
+import android.util.DisplayMetrics;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Scroller;
 
+/**
+ * 自定义viewgroup（5）--可滚动布局，GestureDetector手势监听
+ * https://blog.csdn.net/qq_18148011/article/details/53761587
+ */
 public class MyViewGroup5 extends ViewGroup {
+    //滚动计算辅助类
+    private Scroller mScroller;
+    //屏幕宽度
+    private int screenWidth;
+    //可以移动的最大距离
+    private int mMaxDistance;
+    //自定义手势监听类
+    private ScrollTouchLisener scrollTouchLisener;
+    //手势监听
+    private GestureDetector gestureDetector;
+    private Context context;
+
     public MyViewGroup5(Context context) {
         this(context, null);
     }
@@ -15,20 +37,232 @@ public class MyViewGroup5 extends ViewGroup {
 
     public MyViewGroup5(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
         init();
     }
 
     private void init() {
-
+        //初始化辅助类
+        mScroller = new Scroller(context);
+        //获取屏幕宽度
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(outMetrics);
+        screenWidth = outMetrics.widthPixels;
+        //手势指示器初始化
+        scrollTouchLisener = new ScrollTouchLisener();
+        gestureDetector = new GestureDetector(context, scrollTouchLisener);
     }
 
+//    /**
+//     * 滚动时需要重写的方法，用于控制滚动
+//     */
+//    @Override
+//    public void computeScroll() {
+//        //判断滚动时候停止
+//        if (mScroller.computeScrollOffset()) {
+//            //滚动到指定的位置
+//            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+//            //这句话必须写，否则不能实时刷新
+//            postInvalidate();
+//        }
+//    }
+
+    /**
+     * 手指触屏事件监听
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            this.onUp(event);
+        }
+        return true;
+    }
+
+    /*
+     *测量方法，测量父布局的宽度和高度
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //重新设置宽高
+        this.setMeasuredDimension(measureWidth(widthMeasureSpec, heightMeasureSpec), measureHeight(widthMeasureSpec, heightMeasureSpec));
+    }
+
+    /**
+     * 测量宽度
+     */
+    private int measureWidth(int widthMeasureSpec, int heightMeasureSpec) {
+        // 宽度
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        //父控件的宽（wrap_content）
+        int width = 0;
+        int childCount = getChildCount();
+
+        //重新测量子view的宽度，以及最大高度
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            width += childWidth;
+        }
+        return modeWidth == MeasureSpec.EXACTLY ? sizeWidth : width;
+    }
+
+    /**
+     * 测量高度
+     */
+    private int measureHeight(int widthMeasureSpec, int heightMeasureSpec) {
+        //高度
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        //父控件的高（wrap_content）
+        int height = 0;
+        int childCount = getChildCount();
+
+        //重新测量子view的宽度，以及最大高度
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            height += childHeight;
+        }
+        height = height / childCount;
+        return modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height;
+    }
+
+    /**
+     * 给子布局设定位置
+     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childLeft = 0;//子View左边的间距
+        int childWidth;//子View的宽度
+        int height = getHeight();//屏幕的宽度
+        int childCount = getChildCount();//子View的数量
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            child.layout(childLeft, 0, childLeft + childWidth, height);
+            childLeft += childWidth;
+        }
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
 
+    /*
+     *按下事件 ACTION_DOWN
+     */
+    public boolean onDown(MotionEvent e) {
+        //如果停止滚动则取消动画（即手指按下就停止滚动）
+        if (!mScroller.isFinished()) {
+            mScroller.abortAnimation();
+        }
+        return false;
+    }
+
+    /*
+     *抬起事件 ACTION_UP
+     */
+    public boolean onUp(MotionEvent e) {
+        //得到最后一个子View
+        View lastChild = getChildAt(getChildCount() - 1);
+        //获取滑动的最大滑动距离（最后一个Child的右边框的坐标减去屏幕的宽度）
+        int finalyChild = (int) (lastChild.getX() + lastChild.getWidth() - screenWidth);
+        mMaxDistance = finalyChild;
+        //如果滑动的距离小于第一个控件的最左边（0）则回弹至（0,0）点
+        if (getScrollX() < 0) {
+            scrollTo(0, 0);
+        }
+        //如果滑动的距离大于最大可滑动距离则滑动到最后一个子View
+        if (getScrollX() >= finalyChild)
+            scrollTo(finalyChild, 0);
+        //刷新界面
+        invalidate();
+        return false;
+    }
+
+    /*=================================================================================*/
+    /*
+     *ACTION_DOWN 、短按不移动
+     */
+    public void onShowPress(MotionEvent e) {
+    }
+
+    /*
+     *短按ACTION_DOWN、ACTION_UP
+     */
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    /*
+     *ACTION_DOWN 、慢滑动
+     */
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        //滚动
+        scrollBy((int) distanceX, 0);
+        return false;
+    }
+
+    // ACTION_DOWN 、长按不滑动
+    public void onLongPress(MotionEvent e) {
+    }
+
+    /*
+     *ACTION_DOWN 、快滑动、 ACTION_UP
+     */
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        mScroller.fling(getScrollX(), 0, (int) -velocityX, 0, 0, mMaxDistance, 0, 0);
+        return false;
+    }
+
+    /**
+     * 自定义手势监听类
+     */
+    private class ScrollTouchLisener implements GestureDetector.OnGestureListener {
+
+        //按下事件
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return MyViewGroup5.this.onDown(e);
+        }
+
+        //单击事件
+        @Override
+        public void onShowPress(MotionEvent e) {
+            MyViewGroup5.this.onShowPress(e);
+        }
+
+        //手指抬起事件
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return MyViewGroup5.this.onSingleTapUp(e);
+        }
+
+        //滚动事件
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return MyViewGroup5.this.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        //长按事件
+        @Override
+        public void onLongPress(MotionEvent e) {
+            MyViewGroup5.this.onLongPress(e);
+        }
+
+        //滑动事件
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return MyViewGroup5.this.onFling(e1, e2, velocityX, velocityY);
+        }
     }
 }
